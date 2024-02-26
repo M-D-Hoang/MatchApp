b659e5dc76ad6244eaf86e2b1ae97b22e5b79003
Brandon Olynyk <brandontab2004@gmail.com>
2024-02-26 09:02:48 -0500

    Add form to sell page

--- client/src/pages/Sell/Sell.js
+++ client/src/pages/Sell/Sell.js
@@ -1,10 +1,21 @@
 //Page for Selling
-import React from "react";
+import {useState} from "react";
+import {ItemForm} from '../../components/Forms/ItemForm.js'
+import {CarForm} from '../../components/Forms/CarForm.js'
+
 
 export function Sell() {
+
+    const [sellJSX, setSellJSX] = useState(<></>)
+  
+
     return (
         <div>
             <h1>Add Your Listing</h1>
+
+            <button onClick={()=>setSellJSX(<CarForm/>)}>Sell Car</button>
+            <button onClick={()=>setSellJSX(<ItemForm/>)}>Sell Misc</button>
+            {sellJSX}
         </div>
     );
 }
