f6e39a82cdc2aa8af242480ddf1a9db0151d9060
Minh Duc Hoang <2003minhduchoang@gmail.com>
2024-02-27 15:05:36 -0500

    edit button

--- client/src/components/DetailedView/ItemInfo.js
+++ client/src/components/DetailedView/ItemInfo.js
@@ -2,10 +2,15 @@
 
 import React from "react";
 
-export function ItemInfo({item}) {
-    return <div className="item-info">
-        <h1>{item.title} - {item.price}$</h1>
-        <p>{item.description}</p>
-        <button>Delete</button>
-        </div>;
+export function ItemInfo({ item }) {
+    return (
+        <div className="item-info">
+            <h1>
+                {item.title} - {item.price}$
+            </h1>
+            <p>{item.description}</p>
+            <button>Delete</button>
+            <button>Edit</button>
+        </div>
+    );
 }
