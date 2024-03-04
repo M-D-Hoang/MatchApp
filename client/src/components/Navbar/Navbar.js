c8b40fb1a8412b2bc90755baef8e43d5de62c54d
Brandon Olynyk <brandontab2004@gmail.com>
2024-03-04 18:46:36 -0500

    Add profile button to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -17,7 +17,9 @@ export function Navbar() {
                 </Link>
             </div>
             <div className="title">
-                <h1>Fake Market Place</h1>
+                <Link to="/">
+                    <h1>Fake Market Place</h1>
+                </Link>
             </div>
             <div className="link-container">
                 <div className="link">
@@ -25,6 +27,9 @@ export function Navbar() {
                 </div>
                 <div className="link">
                     <Link to="/your-listings">Your Listings</Link>
+                </div>
+                <div className="link">
+                    <Link to="/my-page"><img className='navbar-pfp' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTTiQ8Yl6XjZK9qjqoqztkUDOXXerRr7Kp0z38NwfdYQ&s' alt='my-account'></img></Link>
                 </div>
             </div>
         </nav>
