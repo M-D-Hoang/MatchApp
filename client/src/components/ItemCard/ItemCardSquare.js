a91dfa222caad00a1a22f922d729aab3985f75a7
Brandon Olynyk <brandontab2004@gmail.com>
2024-03-04 22:20:50 -0500

    Fix bad placeholder data

--- client/src/components/ItemCard/ItemCardSquare.js
+++ client/src/components/ItemCard/ItemCardSquare.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./ItemCardSquare.css";
 
 export function ItemCardSquare({ item, showHandler }) {
+    console.log(item)
     return (
         <div className="item-card" onClick={showHandler}>
-            <img className="preview-image" src={item.imageURIs[0]} alt={item.title}></img>
+            {item.imageURIs !== undefined && <img className="preview-image" src={item.imageURIs[0]} alt={item.title}></img>}
             <div className="preview-text">
                 <p className="title">{item.title}</p>
                 <p className="price">{item.price} $</p>
--- client/src/pages/UserProfile/UserProfile.js
+++ client/src/pages/UserProfile/UserProfile.js
@@ -1,20 +1,82 @@
+import { useEffect, useState } from "react";
+import { ItemCardSquare } from "../../components/ItemCard/ItemCardSquare";
 
-const exampleUser={
-    _id:'user1026',
-    username:'bobzilla1289',
-    lastName:'Bob',
-    firstName:'Billy',
-    birthday:'Febuary 17 2004',
-    gender:'Male',
-    email:'pewdiepie@gmail.com',
-    phoneNumber:'123-123-1234',
-    picture:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTTiQ8Yl6XjZK9qjqoqztkUDOXXerRr7Kp0z38NwfdYQ&s',
-    type:'buyer'
+
+const exampleUser = {
+    _id: 'user1026',
+    username: 'bobzilla1289',
+    lastName: 'Bob',
+    firstName: 'Billy',
+    birthday: 'Febuary 17 2004',
+    gender: 'Male',
+    email: 'pewdiepie@gmail.com',
+    phoneNumber: '123-123-1234',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTTiQ8Yl6XjZK9qjqoqztkUDOXXerRr7Kp0z38NwfdYQ&s',
+    type: 'buyer'
 }
 
-export function UserPage(){
+export function UserPage() {
     //for the real thing, we would have user passed in as a prop
     const user = exampleUser
+    const [userItems, setUserItems] = useState([])
+    useEffect(() => {
+        //get products by user
+        setUserItems([
+            {
+              
+                    "_id": "65dd042f642427fd5770228a",
+                    "ownerID": "user9381",
+                    "title": "NERF Nstrike Snapfire",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dol",
+                    "price": 56.79,
+                    "imageURIs": [
+                        "https://m.media-amazon.com/images/I/71fM1bvB8iL._AC_UL320_.jpg"
+                    ],
+                    "condition": "fair",
+                    "extraField": "none",
+                    "category": "Games & Toys",
+                    "__v": 0
+                
+            },
+            {
+                
+                    "_id": "65dd042f642427fd5770228a",
+                    "ownerID": "user9381",
+                    "title": "NERF Nstrike Snapfire",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dol",
+                    "price": 56.79,
+                    "imageURIs": [
+                        "https://m.media-amazon.com/images/I/71fM1bvB8iL._AC_UL320_.jpg"
+                    ],
+                    "condition": "fair",
+                    "extraField": "none",
+                    "category": "Games & Toys",
+                    "__v": 0
+                
+            },
+            {
+               
+                    "_id": "65dd042f642427fd5770228a",
+                    "ownerID": "user9381",
+                    "title": "NERF Nstrike Snapfire",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dol",
+                    "price": 56.79,
+                    "imageURIs": [
+                        "https://m.media-amazon.com/images/I/71fM1bvB8iL._AC_UL320_.jpg"
+                    ],
+                    "condition": "fair",
+                    "extraField": "none",
+                    "category": "Games & Toys",
+                    "__v": 0
+                
+            }
+
+        ])
+    }, [])
+
+    const listingJSX = userItems.map((item) => {
+        return (<ItemCardSquare item={item} />);
+    })
 
     return (<div>
         <h1>{user.username}</h1>
@@ -29,7 +91,7 @@ export function UserPage(){
         </div>
         <div>
             <h1>Items</h1>
-            
+            {listingJSX}
         </div>
     </div>);
 }
