248ca07d14340fe9ff48e7349b86f595d8ba4658
Vasilii Iurev <yuryev.v@Vasiliis-Macbook-Pro.local>
2024-03-05 14:09:34 -0500

    update postImage to handle one or multiple images correctly

--- server/controllers/imagesController.js
+++ server/controllers/imagesController.js
@@ -17,19 +17,29 @@ const blobService = new BlobServiceClient(
 const containerClient = blobService.getContainerClient(containerName);
 
 exports.postImage = asyncHandler(async (req, res) => {
-  const images = Array.from(req.files.image);
+  let images = [];
+  // if multiple images uploaded, it's an array
+  // else it's an object
+  if (req.files.image.length) {
+    images = Array.from(req.files.image);
+  } else {
+    images.push(req.files.image);
+  }
+  const urls = [];
   // create promises to upload images
-  const uploadPromises = images.map(async (image, i) => {
+  const uploadPromises = images.map(async (image) => {
     const path = image.name;
     const blobClient = containerClient.getBlockBlobClient(path);
     const options = {blobHTTPHeaders: {blobContentType: image.mimetype}};
     await blobClient.uploadData(image.data, options);
     const fullURL = blobPublicUrl + path;
-    // Assign URL to the array.
-    res.locals.listing.imageURIs[i] = fullURL; 
+    // add URL to the new array
+    urls.push(fullURL);
   });
   // Wait for all image upload promises to resolve.
   await Promise.all(uploadPromises);
+  // set new images urls
+  res.locals.listing.imageURIs = urls;
   // update db row
   // check if car
   if (req.body.mileage){
