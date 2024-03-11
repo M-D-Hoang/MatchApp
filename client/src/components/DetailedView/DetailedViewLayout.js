5c2c48b7892e98918df1a5dca9c56165b7900d8b
Vasilii Iurev <yuryev.v@Vasiliis-Macbook-Pro.local>
2024-03-11 11:28:06 -0400

    move deleteItemButton handler down to detailed view component

--- client/src/components/DetailedView/DetailedViewLayout.js
+++ client/src/components/DetailedView/DetailedViewLayout.js
@@ -8,7 +8,6 @@ import { Carousel } from 'react-responsive-carousel';
 
 export function DetailedView({ onExit, item }) {
     const navigate = useNavigate();
-
     // prepare images array
     const images = [];
     // set placeholder if listing has no images
@@ -20,45 +19,9 @@ export function DetailedView({ onExit, item }) {
         images.push(<><img src={img} alt="preview"></img></>);
     });
 
-    const isCar = item.make !== undefined;
-
-    const onDeleteClicked = async () => {
-        const respJSON = JSON.stringify({ _id: item._id });
-
-        var resp = undefined;
-
-        if (isCar) {
-            resp = await fetch("/api/listings/cars", {
-                method: "DELETE",
-                headers: { "Content-Type": "application/json" },
-                body: respJSON,
-            });
-        } else {
-            resp = await fetch("/api/listings/items", {
-                method: "DELETE",
-                headers: { "Content-Type": "application/json" },
-                body: respJSON,
-            });
-        }
-
-        if (resp.status === 204) {
-            //placeholder reload, we'd navigate to a page once we have proper nav paths done
-            // eslint-disable-next-line no-restricted-globals
-            location.reload();
-        } else {
-            //placeholder alert, should be something nicer later
-            const json = await resp.json();
-            alert(`Something went wrong while deleting the entry: ${json}`);
-        }
-        //The below should work when we have routers for individual items working.
-        //For now, it works only once.
-        //navigate("/");
-    };
-
     const handleFullViewURL = () => {
         navigate("/fullview/?itemId=" + item._id, { state: { data: item } });
     }
-
     return (
         <div className={"overlay"} onClick={onExit}>
             <div className={"detailed-view"}>
@@ -66,7 +29,7 @@ export function DetailedView({ onExit, item }) {
                     <Carousel className="carousel" infiniteLoop={true}>{images}</Carousel>
                 </div>
                 <div className="item-info-container">
-                    <ItemInfo onDeleteClicked={onDeleteClicked} item={item} />
+                    <ItemInfo item={item} />
                     <button onClick={handleFullViewURL}>Full View</button>
                 </div>
             </div>
