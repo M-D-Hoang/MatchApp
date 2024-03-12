ef5284ce7dc30a84416444cb8707c761e6968c78
Brandon Olynyk <brandontab2004@gmail.com>
2024-03-12 13:28:45 -0400

    Add updated logo to meta & navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import logo from "../../assets/images/logo-temp.png";
+import logo from "../../assets/images/logo512.png";
 import { Link } from "react-router-dom";
 
 /**
