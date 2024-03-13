66436fcb5f72c6af84d3e9f11da86060c6ed5099
Brandon Olynyk <brandontab2004@gmail.com>
2024-03-13 10:53:04 -0400

    Combine userpage & googlelogin

--- client/src/components/Login/LoginLayout.js
+++ client/src/components/Login/LoginLayout.js
@@ -1,63 +1,100 @@
 import React from "react";
 // GoogleLogou
-import {GoogleLogin} from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import { useEffect, useState } from "react";
-import toast, {Toaster} from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 // import { response } from "../../../../server/server";
+import { Link } from "react-router-dom";
 
 export function Login() {
 
-const [userInfo, setUserInfo] = useState(null)
-useEffect(() => {
-  const checkAuth = async () => {
+  const [userInfo, setUserInfo] = useState(null)
+
+  useEffect(() => {
+    const checkAuth = async () => {
       try {
-          const resp = await fetch('/api/users/check-auth', {
-              method: 'GET',
-              credentials: "include"
-          })
+        const resp = await fetch('/api/users/check-auth', {
+          method: 'GET',
+          credentials: "include"
+        })
 
-          if(!resp.ok) {
-              return
-          }
+        if (!resp.ok) {
+          return
+        }
 
-          const data = await resp.json()
-          setUserInfo(data.data)
-      } catch(e) {
-          console.error(e)
+        const data = await resp.json()
+        console.warn(data)
+        setUserInfo(data)
+      } catch (e) {
+        console.error(e)
       }
-  }
+    }
 
-  checkAuth()
-}, []);
+    checkAuth()
+  }, []);
 
-const handleLogin = async (response) => {
-  const resp = await fetch('/api/users/login', {
+  const handleLogin = async (response) => {
+    const resp = await fetch('/api/users/login', {
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(response),
       credentials: 'include'
-  })
-  if(!resp.ok) {
+    })
+    if (!resp.ok) {
       toast.error('Failed to login')
       return
+    }
+    const data = await resp.json()
+    
+    setUserInfo(data)
+    toast.success('Login Successful')
   }
-  const data = await resp.json()
-  setUserInfo(data.data)
-  toast.success('Login Successful')
-}
-  return (
-    <div className="login-page">
-      <GoogleLogin onSuccess={handleLogin}
+
+
+  let displayableJSX = <LoggedInUserButton/>
+  if(userInfo === null){
+    displayableJSX = (
+      <GoogleLogin 
+        onSuccess={handleLogin}
         onError={() => toast.error('Login failed')}
-        useOneTap={true}
-      />
-      <h1>{userInfo && userInfo.username}</h1>
+        useOneTap={true}/>
+    );
+  }
+
+  console.log(displayableJSX);
+  return (
+
+    <div className="link-container">
+      {displayableJSX}
       <Toaster
         position="top-right"
         reverseOrder={false}
       />
     </div>
+  );
+}
+
+
+function LoggedInUserButton(){
+  return(
+    //Link to User Page & Sell Button
+        <div className="link-container">
+          <div className="link">
+            <Link to="/sell">Sell</Link>
+          </div>
+          <div className="link">
+            <p onClick={()=>{alert('Under construction')}}>Logout</p>
+          </div>
+          <div className="link pfp-container">
+            <Link className="pfp-container-link" to="/my-page">
+              <img
+                className="navbar-pfp"
+                src="https://cdn.discordapp.com/emojis/383621124854120449.webp?size=1024&quality=lossless&name=bolus"
+                alt="my-account"></img>
+            </Link>
+          </div>
+        </div>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -11,9 +11,13 @@ import { Link } from "react-router-dom";
  * @return {JSX.Element} The rendered navigation bar.
  */
 export function Navbar(props) {
+
+    
+    
     return (
         <nav className="navbar">
             <div className="logo">
+                
                 <Link to="/">
                     <img src={logo} alt="logo"></img>
                 </Link>
@@ -24,19 +28,7 @@ export function Navbar(props) {
                 </Link>
             </div>
             <Login setUsename={props.setUsername} />
-            <div className="link-container">
-                <div className="link">
-                    <Link to="/sell">Sell</Link>
-                </div>
-                <div className="link pfp-container">
-                    <Link className="pfp-container-link" to="/my-page">
-                        <img
-                            className="navbar-pfp"
-                            src="https://cdn.discordapp.com/emojis/383621124854120449.webp?size=1024&quality=lossless&name=bolus"
-                            alt="my-account"></img>
-                    </Link>
-                </div>
-            </div>
+            
         </nav>
     );
 }
