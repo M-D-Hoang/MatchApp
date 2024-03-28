b22d2571c5ce04e6a31981f55dac17a11f445034
Brandon Olynyk <brandontab2004@gmail.com>
2024-03-27 21:30:42 -0400

    Advance more on AddressPicker UI element

--- client/src/components/Location/AddressPicker.js
+++ client/src/components/Location/AddressPicker.js
@@ -1,12 +1,26 @@
-function AddressTextField({setAddress}){
+import { useState } from "react";
+import ReactLoading from 'react-loading';
+
+
+
+function AddressTextField({setCoordinates}){
+
+  const [address, setAddress] = useState('')
+  const [isLoading, setIsLoading] = useState(false);
+  //Set the address state var
+  const onInputChange = (e) => {
+    const value = e.target.value;
+    setAddress(value);
+  }
 
   const checkAddress = (e) => {
-
+    //fetch(`https://geocode.maps.co/search?q=${address}&api_key=${LOCATION_KEY}`)
   }
 
   return(
     <div>
-      <label>Address for Pickup: <input type='text'></input></label><button onClick={checkAddress}>Check</button>
+      <ReactLoading className="loading-bar" type={"spin"} color={"#58cc77"} height={16} width={16} />
+      <label>Address for Pickup: <input type='text' onChange={onInputChange}></input></label><button onClick={checkAddress}>Check</button>
     </div>
   );
 }
