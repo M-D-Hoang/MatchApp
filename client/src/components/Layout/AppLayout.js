2351516ec66bfb4c498dd25448c0daac25651b0f
Minh Duc Hoang <2003minhduchoang@gmail.com>
2024-03-28 18:37:12 -0400

    standardized css: colors and some recurring elements

--- client/src/components/Layout/AppLayout.js
+++ client/src/components/Layout/AppLayout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
-import "./AppLayout.css";
 
 /**
  * Layout component that renders a Navbar and an Outlet (current page) inside App.
