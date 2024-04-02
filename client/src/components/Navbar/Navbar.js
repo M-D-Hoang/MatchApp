4b16502175053737f9a73288456df433e63419e3
Brandon Olynyk <brandontab2004@gmail.com>
2024-04-02 14:08:26 -0400

    Cull navbar data on certain width

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -19,6 +19,11 @@ export function Navbar(props) {
     const [t, i18n] = useTranslation("global");
     const [isDark, setIsDark] = useState(false);
 
+    const { height, width } = useWindowDimensions();
+    const mobileWidth = 621; //Width the window will need to be less than to go into mobile ver.
+
+
+
     useEffect(() => {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             setIsDark(true);
@@ -39,29 +44,60 @@ export function Navbar(props) {
     };
 
     return (
-        <nav className="navbar">
-            <div className="logo">
-                <Link to="/">
-                    <img className="logo" src={logo} alt="logo"></img>
-                </Link>
-            </div>
-            <div className="title">
-                <Link to="/">
-                    <h1 className="navbar-title">MatchApp</h1>
-                </Link>
-            </div>
-            <div id="language-selector">
-                <p onClick={() => handleChangeLanguage("en")}>EN</p>
-                <p>|</p>
-                <p onClick={() => handleChangeLanguage("fr")}>FR</p>
-            </div>
-            <Toggle
-                checked={isDark}
-                onChange={({ target }) => handleDarkMode(target.checked)}
-                icons={{ checked: "🌙", unchecked: "🔆" }}
-                aria-label="Dark mode toggle"
-            />
-            <Login setUsename={props.setUsername} pfpURL={props.pfpURL} setPfpURL={props.setPfpURL} />
+        <nav>
+          {width} - {!(width <= mobileWidth) && "nah"}
+            {!(width <= mobileWidth) ?
+                <nav className="navbar"><div className="logo">
+                    <Link to="/">
+                        <img className="logo" src={logo} alt="logo"></img>
+                    </Link>
+                </div><div className="title">
+                        <Link to="/">
+                            <h1 className="navbar-title">MatchApp</h1>
+                        </Link>
+                    </div><div id="language-selector">
+                        <p onClick={() => handleChangeLanguage("en")}>EN</p>
+                        <p>|</p>
+                        <p onClick={() => handleChangeLanguage("fr")}>FR</p>
+                    </div><Toggle
+                        checked={isDark}
+                        onChange={({ target }) => handleDarkMode(target.checked)}
+                        icons={{ checked: "🌙", unchecked: "🔆" }}
+                        aria-label="Dark mode toggle" /><Login setUsename={props.setUsername} pfpURL={props.pfpURL} setPfpURL={props.setPfpURL} />
+                </nav>    
+        : 
+        <nav className="navbar"><div className="logo">
+                    <Link to="/">
+                        <img className="logo" src={logo} alt="logo"></img>
+                    </Link>
+                </div><Login setUsename={props.setUsername} pfpURL={props.pfpURL} setPfpURL={props.setPfpURL} isMobile={!(width <= mobileWidth)} />
+                </nav>    
+        }
         </nav>
     );
+}
+
+//Window Dimension Functions
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
+
+export default function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
 }
